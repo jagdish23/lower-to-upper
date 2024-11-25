7c61290b56d0d27b730ecb776fda8d03ba61c56f
unknown <jagdishtiwari0582@gmail.com>
2024-11-25 13:27:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Textfrom } from './Textfrom'; 
+// In src/index.js or src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container my-3' >
+    <Textfrom heading="Enter the text to analyse"/> 
     </div>
+    
+    </>
   );
 }
 
---
+++ src/Textfrom.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+
+
+export  function Textfrom(props){
+
+    const handleUpclick=()=>{
+        console.log("UpperCase was clicked" + text);
+        let newtext=text.toUpperCase();
+        setText(newtext);
+    }
+    const handleOnChange=(event)=>{
+        console.log('on change');
+        setText(event.target.value)
+    }
+
+    const [text,setText]=useState('Enter text here')
+    return(
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+            </div>
+            <button className='btn btn-primary' onClick={handleUpclick}>Convert to uppercase</button>
+        </div>
+    )
+}
