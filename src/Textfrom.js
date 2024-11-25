f39ae44c582a7ed9be5ea825e472654c86207e2f
unknown <jagdishtiwari0582@gmail.com>
2024-11-25 16:04:24 +0530

    added navbar

--- src/Textfrom.js
+++ src/Textfrom.js
@@ -24,6 +24,44 @@ export  function Textfrom(props){
     const [text,setText]=useState('Enter text here')
     return(
         <>
+                                    <div>
+                                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="#">Navbar</a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto mb-100 mb-lg-9">
+                                    <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a className="dropdown-item" href="#">Action</a></li>
+                                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                                        <li> className="dropdown-divider"</li>
+                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                    </li>
+                                    <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                    </li>
+                                </ul>
+                                <form className="d-flex" role="search">
+                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button className="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                                </div>
+                            </div>
+                            </nav>
+                                    </div>
         <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
